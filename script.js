6d198337d3d4eece972d661fc73f6d13c9678eaa
Javier Brola <javib.2711@gmail.com>
2023-03-17 01:09:29 -0300

    mas cerca del fin

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ let botones = document.querySelectorAll(".agrcarr");
 let carrito = document.querySelector("#carrito");
 let numcarr = document.querySelector("#numcarr")
 let numero = (parseInt(numcarr.textContent));
-console.dir(numcarr)
+
 for (const boton of botones) {
     boton.addEventListener("click", () =>{
         numero= numero +1;
         numcarr.textContent= numero;
+        numcarr.classList.remove("visually-hidden");
     });
 
 
 }
+
