95f859a193e110e3e9df896afd061958a9cc3670
Javier Brola <javib.2711@gmail.com>
2023-03-20 08:49:46 -0300

    finalizo responsive, empiezo js

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let botones = document.querySelectorAll(".agrcarr");
 let carrito = document.querySelector("#carrito");
 let numcarr = document.querySelector("#numcarr")
 let numero = (parseInt(numcarr.textContent));
+let nombre = document.querySelector("#nomb");
+let tel = document.querySelector("#tel");
+let mail = document.querySelector("#mail");
+let consulta = document.querySelector("consul");
+let enviar = document.querySelector("#enviar");
 
 for (const boton of botones) {
     boton.addEventListener("click", () =>{
@@ -11,3 +16,9 @@ for (const boton of botones) {
     });
 }
 
+enviar.addEventListener("click", ()=>{
+    let nomcli = nombre.value;
+    let telcli = tel.value;
+    let mailcli = cli.value;
+    let concli = consulta.value;
+})
